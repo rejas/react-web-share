c65151c861967b5cdfce7d44f72734a35191c835
Veeck <github@veeck.de>
2025-05-24 10:09:19 +0200

    remove storybook (#1)
    Co-authored-by: veeck <gitkraken@veeck.de>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,6 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "plugin:storybook/recommended",
   ],
   rules: {
     "@typescript-eslint/ban-ts-comment": "off",
